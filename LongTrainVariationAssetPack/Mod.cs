d9333e4e0faf28618fbe738d29439bc87de4006d
Mimonsi <konstantin-schwarze@outlook.de>
2025-01-29 00:57:15 +0100

    Initial commit

---
+++ LongTrainVariationAssetPack/Mod.cs
@@ -0,0 +1,22 @@
+﻿using Colossal.Logging;
+using Game;
+using Game.Modding;
+using Game.SceneFlow;
+
+namespace LongTrainVariationAssetPack
+{
+    public class Mod : IMod
+    {
+        public static ILog log = LogManager.GetLogger($"AssetPacksManager.Packs")
+            .SetShowsErrorsInUI(false);
+
+        public void OnLoad(UpdateSystem updateSystem)
+        {
+            log.Info("Loading Asset Pack " + nameof(LongTrainVariationAssetPack));
+        }
+
+        public void OnDispose()
+        {
+        }
+    }
+}
